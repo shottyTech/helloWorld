3f69f229fe968835dbf537a94143434d3553a82b
Shotande Kehinde <hussienk2011@gmail.com>
2025-04-01 21:01:30 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 function App() {
+  
   return (
+    // Objective:- The goal of this assessment is to set up a basic React project and render a simple "Hello World" message in the web browser 
     <div>
       <h1>Hello world</h1>
     </div>
